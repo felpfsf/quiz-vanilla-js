c8b46b5bda187540fa91bf88ddbdbcc47dd2684f
felpfsf <feferreira.sf@gmail.com>
2022-08-01 21:42:43 -0300

    questions array

---
+++ js/data.js
@@ -0,0 +1,317 @@
+export const questions =[
+  {
+    'question': 'Qual bicho transmite Doença de Chagas?',
+    'answers': [
+      {
+        'answer': 'Abelha',
+        'correct': false,
+      },
+      {
+        'answer': 'Barata',
+        'correct': false,
+      },
+      {
+        'answer': 'Pulga',
+        'correct': false,
+      },
+      {
+        'answer': 'Barbeiro',
+        'correct': true,
+      },
+    ]
+  },
+  {
+    'question': 'Qual fruto é conhecido no Norte e Nordeste como "jerimum"?',
+    'answers': [
+      {
+        'answer': 'Caju',
+        'correct': false,
+      },
+      {
+        'answer': 'Abóbora',
+        'correct': true,
+      },
+      {
+        'answer': 'Chuchu',
+        'correct': false,
+      },
+      {
+        'answer': 'Côco',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Qual é o coletivo de cães?',
+    'answers': [
+      {
+        'answer': 'Matilha',
+        'correct': true,
+      },
+      {
+        'answer': 'Rebanho',
+        'correct': false,
+      },
+      {
+        'answer': 'Alcateia',
+        'correct': false,
+      },
+      {
+        'answer': 'Manada',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Qual é o triângulo que tem todos os lados diferentes?',
+    'answers': [
+      {
+        'answer': 'Equilátero',
+        'correct': false,
+      },
+      {
+        'answer': 'Isóceles',
+        'correct': false,
+      },
+      {
+        'answer': 'Escaleno',
+        'correct': true,
+      },
+      {
+        'answer': 'Trapézio',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Quem compôs o Hino da Independência?',
+    'answers': [
+      {
+        'answer': 'Dom Pedro I.',
+        'correct': true,
+      },
+      {
+        'answer': 'Manuel Bandeira',
+        'correct': false,
+      },
+      {
+        'answer': 'Castro Alves',
+        'correct': false,
+      },
+      {
+        'answer': 'Carlos Gomes',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Qual é o antônimo de "malograr"?',
+    'answers': [
+      {
+        'answer': 'Perder',
+        'correct': false,
+      },
+      {
+        'answer': 'Fracassar',
+        'correct': false,
+      },
+      {
+        'answer': 'Conseguir',
+        'correct': true,
+      },
+      {
+        'answer': 'Desprezar',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Em que país nasceu Carmem Miranda?',
+    'answers': [
+      {
+        'answer': 'Brasil',
+        'correct': false,
+      },
+      {
+        'answer': 'Espanha',
+        'correct': false,
+      },
+      {
+        'answer': 'Portugal',
+        'correct': true,
+      },
+      {
+        'answer': 'Argentina',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Qual foi o último Presidente do período da ditadura militar no Brasil?',
+    'answers': [
+      {
+        'answer': 'Costa e Silva',
+        'correct': false,
+      },
+      {
+        'answer': 'João Figueiredo',
+        'correct': true,
+      },
+      {
+        'answer': 'Ernesto Geisel',
+        'correct': false,
+      },
+      {
+        'answer': 'Emílio Médici',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Seguindo a sequência do baralho, qual carta vem depois do dez?',
+    'answers': [
+      {
+        'answer': 'Rei',
+        'correct': false,
+      },
+      {
+        'answer': 'Valete',
+        'correct': true,
+      },
+      {
+        'answer': 'Nove',
+        'correct': false,
+      },
+      {
+        'answer': 'Ás',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'O adjetivo "venoso" está relacionado a:',
+    'answers': [
+      {
+        'answer': 'Vela',
+        'correct': false,
+      },
+      {
+        'answer': 'Vento',
+        'correct': false,
+      },
+      {
+        'answer': 'Vênia',
+        'correct': false,
+      },
+      {
+        'answer': 'Veia',
+        'correct': true,
+      },
+    ]
+  },
+  {
+    'question': 'Que nome se dá à purificação por meio da água?',
+    'answers': [
+      {
+        'answer': 'Abolição',
+        'correct': false,
+      },
+      {
+        'answer': 'Abnegação',
+        'correct': false,
+      },
+      {
+        'answer': 'Ablução',
+        'correct': true,
+      },
+      {
+        'answer': 'Abrupção',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Qual montanha se localiza entre a fronteira do Tibet com o Nepal?',
+    'answers': [
+      {
+        'answer': 'Monte Everest',
+        'correct': true,
+      },
+      {
+        'answer': 'Monte Carlo',
+        'correct': false,
+      },
+      {
+        'answer': 'Monte Fuji',
+        'correct': false,
+      },
+      {
+        'answer': 'Monte Branco',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Em que parte do corpo se encontra a epiglote?',
+    'answers': [
+      {
+        'answer': 'Estômago',
+        'correct': false,
+      },
+      {
+        'answer': 'Pâncreas',
+        'correct': false,
+      },
+      {
+        'answer': 'Rim',
+        'correct': false,
+      },
+      {
+        'answer': 'Boca',
+        'correct': true,
+      },
+    ]
+  },
+  {
+    'question': 'A compensação por perda é chamada de...',
+    'answers': [
+      {
+        'answer': 'Déficit',
+        'correct': false,
+      },
+      {
+        'answer': 'Indenização',
+        'correct': true,
+      },
+      {
+        'answer': 'Indexação',
+        'correct': false,
+      },
+      {
+        'answer': 'Indébito',
+        'correct': false,
+      },
+    ]
+  },
+  {
+    'question': 'Como se chama o alimento extraído pelas plantas do solo?',
+    'answers': [
+      {
+        'answer': 'Celulose',
+        'correct': false,
+      },
+      {
+        'answer': 'Seiva',
+        'correct': true,
+      },
+      {
+        'answer': 'Mitose',
+        'correct': false,
+      },
+      {
+        'answer': 'Clorofila',
+        'correct': false,
+      },
+    ]
+  },
+]
